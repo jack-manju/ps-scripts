d7024128a807b9df55c7bcbbe1df30c8d13bcfb3
Gaurav Parmar <gaurav.parmar@isobar.com>
2018-03-01 16:52:29 +0530

    copy only today's files to network location

---
+++ Daily_Copy_DB_Backup.ps1
@@ -0,0 +1,6 @@
+$today = get-date -Format yyyyMMdd
+
+$source = "\\172.21.28.39\TFS_Backup"
+$dest = "\\172.21.24.17\India Order Dynamics\Pune-OD Infrastructure\TFS_Backup"
+
+robocopy  $source \$dest /s /maxage:$today
